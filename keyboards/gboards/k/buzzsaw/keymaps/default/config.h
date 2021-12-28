56cc6a35bcefc687ba12f5fb76647887724690e1
BuildTools <unconfigured@null.spigotmc.org>
2021-12-27 21:14:32 -0500

    Added my modified buzzsaw code

---
+++ keyboards/gboards/k/buzzsaw/keymaps/default/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define COMBO_COUNT 0
+
+#define IGNORE_MOD_TAP_INTERRUPT
---
+++ keyboards/gboards/k/buzzsaw/keymaps/default/rules.mk
@@ -0,0 +1,26 @@
+#----------------------------------------------------------------------------
+# make gergo:germ:dfu
+# Make sure you have dfu-programmer installed!
+#----------------------------------------------------------------------------
+# Firmware options
+BALLER = no 			# Enable to ball out
+BALLSTEP = 20  			# Multiple in px to move, multiplied by layer number
+SCROLLSTEP = 1 			# Lines to scroll with ball
+MOUSEKEY_ENABLE = yes  		# Mouse keys(+4700), needed for baller
+COMBO_ENABLE = yes
+CONSOLE_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+
+#Debug options
+VERBOSE 		 = no
+DEBUG_MATRIX_SCAN_RATE   = no
+DEBUG_BALLER 		 = no
+DEBUG_MATRIX		 = no
+
+# A bunch of stuff that you shouldn't touch unless you
+# know what you're doing.
+#
+# No touchy, capiche?
+ifeq ($(strip $(DEBUG_MATRIX)), yes)
+    OPT_DEFS += -DDEBUG_MATRIX
+endif
